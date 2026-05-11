25d799539ca83efee3cafee534e7ab048cab4292
Panhong807 <panhong.25@intl.zju.edu.cn>
2026-05-11 21:00:45 +0800

    Practical 7

--- Practical 7/count_codons.py
+++ Practical 7/count_codons.py
@@ -1,52 +1,32 @@
-# This script reads a FASTA file containing cDNA sequences of Saccharomyces cerevisiae, identifies the longest open reading frame (ORF) for each gene, and determines the stop codon used in that ORF. The results are written to a new FASTA file with the gene name and the stop codon used.
+# This script asks the user for a stop codon, reads the original yeast cDNA FASTA file,
+# finds the longest ORF ending in that stop codon for each gene, counts the in-frame codons
+# upstream of that stop codon, and saves a pie chart of codon usage.
 import matplotlib.pyplot as plt
 stop = input("Enter stop codon (TAA, TAG, TGA): ")
 stop = stop.upper()
-file = open("stop_genes.fa", "r")
+
+if stop not in ["TAA", "TAG", "TGA"]:
+    print("Error: stop codon must be TAA, TAG, or TGA")
+    exit()
+
+file = open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa", "r")
 seq = ""
 codon_count = {}
-for line in file:
-    line = line.strip()
-    if line.startswith(">"):
-        if seq != "":
-            longest_orf = ""
-            for i in range(len(seq) - 2):
-                if seq[i:i+3] == "ATG":
-                    current_orf = ""
-                    for j in range(i, len(seq)-2, 3):
-                        codon = seq[j:j+3]
-                        current_orf = current_orf + codon
-                        if codon == stop:
-                            if len(current_orf) > len(longest_orf):
-                                longest_orf = current_orf
-                            break
-            if longest_orf != "":
-                coding = longest_orf[:-3]
-                for i in range(0, len(coding), 3):
-                    codon = coding[i:i+3]
-                    if codon in codon_count:
-                        codon_count[codon] = codon_count[codon] + 1
-                    else:
-                        codon_count[codon] = 1
-        seq = ""
-    else:
-        seq = seq + line
-if seq != "":
-    longest_orf = ""
 
+
+def count_codons_for_sequence(seq, stop, codon_count):
+    longest_orf = ""
     for i in range(len(seq) - 2):
         if seq[i:i+3] == "ATG":
-
             current_orf = ""
-
             for j in range(i, len(seq)-2, 3):
                 codon = seq[j:j+3]
                 current_orf = current_orf + codon
-
                 if codon == stop:
                     if len(current_orf) > len(longest_orf):
                         longest_orf = current_orf
                     break
+
     if longest_orf != "":
         coding = longest_orf[:-3]
         for i in range(0, len(coding), 3):
@@ -55,6 +35,18 @@ if seq != "":
                 codon_count[codon] = codon_count[codon] + 1
             else:
                 codon_count[codon] = 1
+
+
+for line in file:
+    line = line.strip()
+    if line.startswith(">"):
+        if seq != "":
+            count_codons_for_sequence(seq, stop, codon_count)
+        seq = ""
+    else:
+        seq = seq + line
+if seq != "":
+    count_codons_for_sequence(seq, stop, codon_count)
 file.close()
 labels = []
 sizes = []
@@ -63,8 +55,9 @@ for codon in codon_count:
         labels.append(codon)
         sizes.append(codon_count[codon])
 plt.figure()
-plt.pie(sizes, labels=labels)
+plt.pie(sizes, labels=labels, autopct='%1.1f%%')
 plt.title("Codon usage for " + stop)
+plt.xlabel("Codons")
 plt.savefig("codon_usage_" + stop + ".png")
 plt.close()
 print("Pie chart saved")
--- Practical 7/stop_codons.py
+++ Practical 7/stop_codons.py
@@ -1,54 +1,50 @@
-# This script reads a FASTA file containing cDNA sequences of Saccharomyces cerevisiae, identifies the longest open reading frame (ORF) for each gene, and determines the stop codon used in that ORF. The results are written to a new FASTA file with the gene name and the stop codon used.
+# This script reads a FASTA file containing cDNA sequences of Saccharomyces cerevisiae,
+# identifies genes that contain at least one in-frame stop codon in an ORF that starts with ATG,
+# and writes them to stop_genes.fa with only the gene name and the stop codon types found.
+
+STOP_CODONS = ["TAA", "TAG", "TGA"]
+
+
+def find_stop_codons(seq):
+    found_stops = set()
+
+    for i in range(len(seq) - 2):
+        if seq[i:i + 3] == "ATG":
+            for j in range(i, len(seq) - 2, 3):
+                codon = seq[j:j + 3]
+                if codon in STOP_CODONS:
+                    found_stops.add(codon)
+                    break
+
+    return found_stops
+
+
 infile = open("Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa", "r")
 outfile = open("stop_genes.fa", "w")
 seq = ""
 gene_name = ""
+
 for line in infile:
     line = line.strip()
     if line.startswith(">"):
         if seq != "":
-            longest_length = 0
-            best_stop = ""
-            for i in range(len(seq) - 2):
-                if seq[i:i+3] == "ATG":
-                    j = i
-                    length = 0
-                    while j < len(seq) - 2:
-                        codon = seq[j:j+3]
-                        length += 3
-                        if codon == "TAA" or codon == "TAG" or codon == "TGA":
-                            if length > longest_length:
-                                longest_length = length
-                                best_stop = codon
-                            break
-                        j += 3
-            if best_stop != "":
-                outfile.write(">" + gene_name + ";" + best_stop + "\n")
+            found_stops = find_stop_codons(seq)
+            if found_stops:
+                stop_text = ",".join(sorted(found_stops))
+                outfile.write(">" + gene_name + ";" + stop_text + "\n")
                 outfile.write(seq + "\n")
         gene_name = line.split()[0][1:]
         seq = ""
     else:
         seq += line
-if seq != "":
-    longest_length = 0
-    best_stop = ""
-    for i in range(len(seq) - 2):
-        if seq[i:i+3] == "ATG":
-            j = i
-            length = 0
-            while j < len(seq) - 2:
-                codon = seq[j:j+3]
-                length += 3
 
-                if codon == "TAA" or codon == "TAG" or codon == "TGA":
-                    if length > longest_length:
-                        longest_length = length
-                        best_stop = codon
-                    break
-                j += 3
-    if best_stop != "":
-        outfile.write(">" + gene_name + ";" + best_stop + "\n")
+if seq != "":
+    found_stops = find_stop_codons(seq)
+    if found_stops:
+        stop_text = ",".join(sorted(found_stops))
+        outfile.write(">" + gene_name + ";" + stop_text + "\n")
         outfile.write(seq + "\n")
+
 infile.close()
 outfile.close()
 
